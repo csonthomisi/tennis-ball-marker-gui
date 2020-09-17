e9e031613380656dfbd5fcb7299054a1960e2b14
askalmas <ask.almas@gmail.com>
2020-09-17 16:25:50 +0200

    label, format

--- ui_components/delete_button.py
+++ ui_components/delete_button.py
@@ -2,10 +2,10 @@ from PySide2 import QtGui, QtCore
 from PySide2.QtWidgets import QPushButton
 
 
-class EditButton(QPushButton):
+class DeleteButton(QPushButton):
     def __init__(self, *args):
         super().__init__(*args)
-        self.setIcon(QtGui.QIcon("edit.jpg"))
+        self.setIcon(QtGui.QIcon("delete.jpg"))
         self.setIconSize(QtCore.QSize(20, 20))
 
     def set_row_column(self, row, col):
